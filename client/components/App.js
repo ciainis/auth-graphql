87da851d87cda27aa19e75d0f989b62b055b55c2
Marco Crippa <m.crippa84@gmail.com>
2019-07-27 16:51:56 +0200

    client setup

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const App = props => {
+  return (
+    <div>
+      <Header />
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+export default App;
