2503103ea0fe63ef8d853d27885c2788e43ad521
Marco Crippa <m.crippa84@gmail.com>
2019-07-28 12:58:16 +0200

    login error handling

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/login';
+import query from '../queries/currentUser';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { errors: [] };
+  }
+
   handleSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password }
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }]
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(err => err.message);
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div>
         <h3>Login</h3>
-        <AuthForm handleSubmit={this.handleSubmit.bind(this)} />
+        <AuthForm
+          handleSubmit={this.handleSubmit.bind(this)}
+          errors={this.state.errors}
+        />
       </div>
     );
   }
